f29386ebc5b8ed4c49dcfe70a6c521fd944b2ed2
kt_boom <kritsada.tan@hotmail.com>
2021-04-28 16:54:24 +0700

    add buyToken function and create some basic test

--- test/BoomSwap.test.js
+++ test/BoomSwap.test.js
@@ -1,6 +1,65 @@
 const Token = artifacts.require('Token')
 const BoomSwap = artifacts.require('BoomSwap')
 
-contract('BoomSwap', (accounts) => {
+require('chai')
+	.use(require('chai-as-promised'))
+	.should()
 
+function tokens(n){
+	return web3.utils.toWei(n, 'ether')
+}
+
+contract('BoomSwap', ([deployer, investor]) => {
+
+	let token, boomSwap
+
+	before(async () => {
+		token = await Token.new()
+		boomSwap = await BoomSwap.new(token.address)
+		await token.transfer(boomSwap.address, tokens('1000000'))
+	})
+
+	describe('Token deployment', async () => {
+		it('contract has BooM Token name', async () => {
+			const name = await token.name()
+			assert.equal(name, 'BooM Token')
+		})
+	})
+
+	describe('BoomSwap deployment', async () => {
+		it('contract has a BooMSwap name', async () => {
+			const name = await boomSwap.name()
+			assert.equal(name, 'BooMSwap Instant Exchange')
+		})
+
+		it('contract has tokens', async () => {
+		let balance = await token.balanceOf(boomSwap.address)
+		assert.equal(balance.toString(), tokens('1000000'))
+		})
+	})
+	
+	describe('buyTokens()', async () => {
+		let result
+
+		before(async () => {
+			result = await boomSwap.buyTokens({ from: investor, value: web3.utils.toWei('1', 'ether')})
+		})
+
+		it('User using BooMSwap buy tokens', async () => {
+			let investorBalance = await token.balanceOf(investor)
+			assert.equal(investorBalance.toString(), tokens('1000'))
+
+			let boomSwapBalance = await token.balanceOf(boomSwap.address)
+			assert.equal(boomSwapBalance.toString(), tokens('999000'))
+			boomSwapBalance = await web3.eth.getBalance(boomSwap.address)
+			assert.equal(boomSwapBalance.toString(), web3.utils.toWei('1', 'Ether'))
+
+			const event = result.logs[0].args
+			assert.equal(event.account, investor)
+			assert.equal(event.token, token.address)
+			assert.equal(event.amount.toString(), tokens('1000').toString())
+			assert.equal(event.rate.toString(), '1000')
+		})
+
+	})
 })
